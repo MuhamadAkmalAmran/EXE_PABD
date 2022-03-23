66967e8e9180a2fcb0e7941c61879da7e777a759
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-03-24 02:16:27 +0700

    Add project files.

---
+++ EXE_PABD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE_PABD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()//membuat koneksi
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=LENOVO\\AKMALAMRAN;database=akmal_exe2;User ID=sa;Password=uzumaki")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Berhasil terhubung ke database!");
+            }
+
+        }
+    }
+}
